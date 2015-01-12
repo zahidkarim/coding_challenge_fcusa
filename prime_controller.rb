265cf15f815989d5b9aa03a9a027f83100dc85d1
ZahidKarim <zahid.jethani@gmail.com>
2015-01-12 14:15:49 -0800

    Adds command for view to print array to screen

--- prime_controller.rb
+++ prime_controller.rb
@@ -5,9 +5,12 @@ x = ARGV[0]
 y = ARGV[1]
 
 new_matrix = Prime.new(columns: x, rows: y)
+show = View.new
 new_matrix.find_primes
 new_matrix.set_top_row
 new_matrix.set_first_column
+new_matrix.fill_in_grid
+show.print_matrix(new_matrix.matrix)
 
 
 
